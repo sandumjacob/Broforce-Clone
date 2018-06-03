201c90a45100946f96bdcdbc319674ec326a108a
sandumjacob <sandumjacobpersonal@gmail.com>
2018-06-03 15:45:34 -0500

    Initial Commit

---
+++ Assets/Scripts/ComplexController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplexController : MonoBehaviour {
+
+	public float maxVelocity;
+	public float jumpTakeOffVelocity;
+	public float minMoveDistance;
+
+	public float gravMod = 1f;
+
+	private bool grounded;
+	private Vector2 targetVelocity;
+	private Vector2 velocity;
+	private Vector2 groundNormal;
+
+
+	private Rigidbody2D rb2d;
+	private Animator animator;
+	private SpriteRenderer sr;
+
+	void onEnable() {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		//velocity += gravMod * Physics2D.gravity * Time.deltaTime;
+		velocity.x = targetVelocity.x;
+
+		grounded = false;
+
+		Vector2 deltaPosition = velocity * Time.deltaTime;
+
+		Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
+
+		Vector2 move = moveAlongGround * deltaPosition.x;
+
+		Movement (move, false);
+
+		move = Vector2.up * deltaPosition.y;
+
+		Movement (move, true);
+	}
+
+	void Movement (Vector2 move, bool yMovement) {
+		float distance = move.magnitude;
+
+		if (distance > minMoveDistance) {
+			
+		}
+
+		rb2d.position = rb2d.position + move.normalized * distance;
+	}
+
+	private void ComputeVelocity() {
+		
+		Vector2 move = Vector2.zero;
+
+		move.x = Input.GetAxis ("Horizontal");
+
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			velocity.y = jumpTakeOffVelocity;
+		} else if (Input.GetButtonUp ("Jump")) {
+			if (velocity.y > 0) {
+				velocity.y = velocity.y * 0.5f;
+			}
+		}
+
+		bool flipSprite = (sr.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+		if (flipSprite) {
+			sr.flipX = !sr.flipX;
+		}
+
+		animator.SetBool ("grounded", grounded);
+		animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxVelocity);
+
+		targetVelocity = move * maxVelocity;
+	}
+
+}
---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour {
+
+	public bool facingRight = true;
+	public bool jump = false;
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+
+	private bool grounded = false;
+
+	private Rigidbody2D rb2d;
+	private Animator animator;
+	private SpriteRenderer spriteRenderer;
+
+	void Start() {
+		rb2d = GetComponent <Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update() {
+		
+		//Should include "grounded"
+		if (Input.GetButtonDown ("Jump")) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate() {
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		//animator.SetFloat ("Speed", Mathf.Abs (horizontal));
+
+		if (horizontal * rb2d.velocity.x < maxSpeed)
+			rb2d.AddForce (Vector2.right * horizontal * moveForce);
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		if (horizontal > 0 && !facingRight)
+			Flip ();
+		else if (hideFlags < 0 && facingRight)
+			Flip ();
+
+		if (jump) {
+			animator.SetTrigger ("Jump");
+			rb2d.AddForce (new Vector2 (0f, jumpForce));
+			jump = false;
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Floor")
+			grounded = true;
+		
+	}
+
+	void OnCollisionExit(Collision collision) {
+		if (collision.gameObject.tag == "Floor")
+			grounded = false;
+	}
+
+}
